2e70d3094847442dbb2bd1225ca0149d4777e4dc
emina-ba96 <omerovic.emina@yahoo.com>
2018-08-01 15:04:12 +0200

    pokusaj

---
+++ v4/vendor/js/js.js
@@ -0,0 +1,28 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("Slide");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 3000); // mjenja sliku na 3 sec
+}
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(44.53, 18.66),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    }
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
+
